a356e56616fd7b2c80e0029987b9b1836bb77c5c
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-07-27 16:34:18 -0700

    .

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,4 +1,4 @@
-import { Application, Sprite, Assets } from 'pixi.js';
+import * as PIXI from 'pixi.js';
 
 let w = window.innerWidth;
 let h = window.innerHeight;
@@ -7,48 +7,72 @@ let app;
 let keys = {};
 let player;
 let keysDiv;
+let character;
+let characterMovement = [];
+let animations;
+
+let loader = new PIXI.Loader();
 
 window.onload = async function () {
-  // The application will create a renderer using WebGL, if possible,
-  // with a fallback to a canvas render. It will also setup the ticker
-  // and the root stage PIXI.Container
-  app = new Application();
 
-  // Wait for the Renderer to be available
-  await app.init({ w, h });
+  app = new PIXI.Application();
 
-  //this is the class _app extends PIXI.Application
+  (async () => {
+    await app.init({
+      // application options
+      width: w, height: h, backgroundColor: 0x7a7a7a
+    });
 
-  app.renderer.resize(w, h)
+    // do pixi things
+    app.renderer.resize(w, h)
 
-  // The application will create a canvas element for you that you
-  // can then insert into the DOM
-  document.body.appendChild(app.canvas);
+    // The application will create a canvas element for you that you
+    // can then insert into the DOM
+    document.body.appendChild(app.canvas);
 
-  // load the texture we need
-  const texture = await Assets.load('bluefront.png');
+    // load the texture we need
+    const texture = await PIXI.Assets.load('bluefront.png');
+    const walkDownTexture = await PIXI.Assets.load([
+        "player_movement.json"  
+    ]);
 
-  // This creates a texture from a 'bunny.png' image
-  player = new Sprite(texture);
+    animations = PIXI.Assets.cache.get('player_movement.json').data.animations;
 
-  // Setup the position of the bunny
-  player.x = app.renderer.width / 2;
-  player.y = app.renderer.height / 2;
+    //character = PIXI.AnimatedSprite.fromFrames(animations["walk_down/walk_down"]);
+    characterMovement["walk_down"] = PIXI.AnimatedSprite.fromFrames(animations["walk_down/walk_down"]);
+    characterMovement["walk_up"] = PIXI.AnimatedSprite.fromFrames(animations["walk_up/walk_up"]);
+    characterMovement["walk_left"] = PIXI.AnimatedSprite.fromFrames(animations["walk_left/walk_left"]);
+    characterMovement["walk_right"] = PIXI.AnimatedSprite.fromFrames(animations["walk_right/walk_right"]);
+
+    character = characterMovement["walk_down"];
+    
+    // configure + start animation:
+    character.animationSpeed = 1 / 6;                     // 6 fps
+    //character.position.set(150, 100); // almost bottom-left corner of the canvas
+
+    // add it to the stage and render!
+    app.stage.addChild(character);
+    
+    //app.stage.addChild(characterMovement["walk_down"], characterMovement["walk_up"], characterMovement["walk_left"], characterMovement["walk_right"] )
+
+    // Setup the position of the bunny
+    character.x = app.renderer.width / 2;
+    character.y = app.renderer.height / 2;
+
+    // Listen for window resize events
+    window.addEventListener('resize', resize);
+
+    //movement event listeners
+    window.addEventListener("keydown", keysdown);
+    window.addEventListener("keyup", keysup);
+
+    app.ticker.add(gameloop);
+
+    keysDiv = document.querySelector("#keys")
+  })()
 
 
-  // Add the bunny to the scene we are building
-  app.stage.addChild(player);
 
-  // Listen for window resize events
-  window.addEventListener('resize', resize);
-
-  //movement event listeners
-  window.addEventListener("keydown", keysdown);
-  window.addEventListener("keyup", keysup);
-
-  app.ticker.add(gameloop);
-
-  keysDiv = document.querySelector("#keys")
 }
 
 //let keysDiv = document.querySelector("#keys");
@@ -57,34 +81,45 @@ function keysdown(e) {
   var keyCode = (window.event) ? e.which : e.keyCode;
   console.log(keyCode);
   keys[keyCode] = true;
-  //console.log(keys[e.keycode])
 }
 
 function keysup(e) {
   var keyCode = (window.event) ? e.which : e.keyCode;
   console.log(keyCode);
   keys[keyCode] = false
+
+  // stop all animation when keys are let go 
+  character.stop();
 }
 
 function gameloop() {
   keysDiv.innerHTML = JSON.stringify(keys)
-
   
-  if(keys['87'] && player.y > 0){
-    player.y -=5;
+  //walk up
+  if (keys['87'] && character.y > 0) {
+    character.textures = characterMovement["walk_up"].textures;
+    character.animationSpeed = 1 / 6;
+    character.visible = true;
+    character.y -= 5;
+    character.play();
   }
-  if(keys["65"]  && player.x > 0){
-    
-    player.x -=5;
+  //walk left
+  if (keys["65"] && character.x > 0) {
+    character.textures =  characterMovement["walk_left"].textures; 
+    character.x -= 5;
+    character.play();
   }
-  if(keys["83"] && player.y < window.innerHeight - 36){
-    console.log(window.innerHeight)
-    console.log(player.y)
-    player.y +=5;
+  //walk down
+  if (keys["83"] && character.y < window.innerHeight - 36) {
+    character.textures =  characterMovement["walk_down"].textures;
+    character.y += 5;
+    character.play();
   }
-  if(keys["68"] && player.x < window.innerWidth - 64){
-    
-    player.x +=5;
+  //walk right
+  if (keys["68"] && character.x < window.innerWidth - 64) {
+    character.textures =  characterMovement["walk_right"].textures;
+    character.x += 5;
+    character.play();
   }
 }
 
